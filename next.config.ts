ce5f9d6baecd1c23f62a6a1356ed2eff22628bc3
Muhammad Ranju <mdranju23@gmail.com>
2026-01-15 22:24:49 +0600

    feat: Initialize Next.js frontend application with Redux Toolkit, API integration, and authentication middleware.

---
+++ next.config.ts
@@ -0,0 +1,43 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost:3000",
+      },
+      {
+        protocol: "http",
+        hostname: "10.10.7.101:3000",
+      },
+      {
+        protocol: "http",
+        hostname: "10.10.7.102",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+  },
+  /* config options here */
+};
+
+export default nextConfig;
